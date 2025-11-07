c955cf09f75067e1b37337f970a7f5e697a0827f
Yogeshwaran K <yogeshwaran.k2021c@vitstudent.ac.in>
2025-11-07 18:19:12 +0530

    Revert "Add chatbot loader endpoint and session updates"
    This reverts commit 44702b95321c58ccfa605baf5d9ec3c8afe9c8b5.

--- models/Customer.js
+++ models/Customer.js
@@ -18,13 +18,6 @@ const customerSchema = new mongoose.Schema({
   },
   session_id: {
     type: String
-  },
-  last_active: {
-    type: Date,
-    default: Date.now
-  },
-  cart_id: {
-    type: String
   }
 }, {
   timestamps: true
